66eb882d6175d55367ab6648e7654856ffc82fce
Kurt Pagal <kurtpagal@gmail.com>
2025-01-25 16:06:34 -0600

    Fixing health error

--- bubbler-/scripts/bullet.gd
+++ bubbler-/scripts/bullet.gd
@@ -1,8 +1,11 @@
 extends Area2D
 class_name bullet
+
 enum bullet_type {normal, white, strawberry, egg} # bullet types
 # Called when the node enters the scene tree for the first time.
+
 const SPEED = 200
+
 func _ready() -> void:
 	pass # Replace with function body.
 
--- bubbler-/scripts/player.gd
+++ bubbler-/scripts/player.gd
@@ -1,22 +1,46 @@
 extends CharacterBody2D
 
-
 const SPEED = 500.0
+const LIFE = 5
+
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var health_bar = $HealthBar
+
+var player_health = LIFE
+
+func _ready() -> void:
+	health_bar.value = player_health
+	health_bar.max_value = LIFE
+	health_bar.visible = true
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_mouse_entered() -> void: #SPECIFICALLY FOR TESTING
+	#player_health -= 1
+	#health_bar.value = player_health
+	health_bar.value -=1
+	$HealthBar.value -= 1
+	
+	if player_health <= 0:
+		_die()
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Vector2.ZERO # stay in place
 	
-	
+	# Apply movement
 	if Input.is_action_pressed("RIGHT"):
+		animated_sprite.play("right_facing")
 		direction.x = SPEED
 	elif Input.is_action_pressed("LEFT"):
+		animated_sprite.play("left_facing")
 		direction.x = -SPEED
 	elif Input.is_action_pressed("DOWN"):
+		animated_sprite.play("down_facing")
 		direction.y = SPEED
-		$Gun.position = (0,0)
 	elif Input.is_action_pressed("UP"):
+		animated_sprite.play("up_facing")
 		direction.y = -SPEED
 	
 	if direction.length() > 0:
@@ -34,3 +58,5 @@ func _physics_process(delta: float) -> void:
 	position.x = clamp(position.x, player_width, viewport.x)
 	position.y = clamp(viewport.y, player_height, position.y)
 	
+func _die() -> void:
+	print("Player Died!")
