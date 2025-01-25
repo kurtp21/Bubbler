2c3920e9a6002980b0a7f8a5d45fa92ca665c608
Dawson Epp (woc945) <mattaya24@gmail.com>
2025-01-25 15:47:22 -0600

    BULLETS WORK YAHOOOO

--- bubbler-/scripts/player.gd
+++ bubbler-/scripts/player.gd
@@ -1,39 +1,30 @@
 extends CharacterBody2D
 
-
 const SPEED = 500.0
 
 var bullet_scene = preload("res://scenes/bullet.tscn")
 var bullets = []
 enum dir {right, left, up, down}
 var facing = dir.down
+
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Vector2.ZERO # stay in place
-	
-	if InputEventMouseButton:
-		var boba = bullet_scene.instantiate()
-		boba.position = $Gun.position
-		add_child(boba)
-		bullets.append(boba)
-		
 	
 	if Input.is_action_pressed("RIGHT"):
 		facing = dir.right
-		direction.x = SPEED
+		direction.x = SPEED*delta
 		$Gun.position = Vector2(32,-32)
 	elif Input.is_action_pressed("LEFT"):
 		facing = dir.left
-		direction.x = -SPEED
+		direction.x = -SPEED*delta
 		$Gun.position = Vector2(-32,-32)
 	elif Input.is_action_pressed("DOWN"):
 		facing = dir.down
-		direction.y = SPEED
+		direction.y = SPEED*delta
 		$Gun.position = Vector2(0,0)
 	elif Input.is_action_pressed("UP"):
 		facing = dir.up
-		direction.y = -SPEED
+		direction.y = -SPEED*delta
 		$Gun.position = Vector2(0, -64)
 	
 	if direction.length() > 0:
@@ -41,13 +32,26 @@ func _physics_process(delta: float) -> void:
 		
 	velocity = direction * SPEED
 	move_and_slide()
-	
-	# Prevent from moing out of bounds
+
+	# Fire bullet only once per click
+	if Input.is_action_just_pressed("shoot"):
+		var boba = bullet_scene.instantiate()
+		boba.global_position = $Gun.global_position
+		if facing == dir.right:
+			boba.global_rotation = deg_to_rad(0)
+		elif facing == dir.left:
+			boba.rotation = deg_to_rad(180)
+		elif facing == dir.up:
+			boba.rotation = deg_to_rad(270)
+		elif facing == dir.down:  
+			boba.rotation = deg_to_rad(90)
+		get_parent().add_child(boba)
+		bullets.append(boba)
+
+	# Prevent player from moving out of bounds
 	var viewport = get_viewport_rect().size
-	
 	var player_width = $CollisionShape2D.shape.get_rect().size.x
 	var player_height = $CollisionShape2D.shape.get_rect().size.y
-	
+
 	position.x = clamp(position.x, player_width, viewport.x)
 	position.y = clamp(position.y, player_height, viewport.y)
-	
