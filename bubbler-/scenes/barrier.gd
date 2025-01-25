a0e6b82bf661ac9e7833c87b12afaef6dd46557d
Dawson Epp (woc945) <mattaya24@gmail.com>
2025-01-25 13:11:33 -0600

    added code so if bullet hits damage is done

--- bubbler-/scenes/barrier.gd
+++ bubbler-/scenes/barrier.gd
@@ -12,5 +12,14 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_mouse_entered() -> void:
+func _on_mouse_entered() -> void: #SPECIFICALLY FOR TESTING
 	$HealthBar.value -= 1 # Replace with function body.
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is bullet:
+		hit(area)
+
+
+func hit(dam: bullet):
+	$HealthBar.value -= 1 #CHange this ti a var when other hullet types are added
