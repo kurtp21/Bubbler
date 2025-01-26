1a7b88d35acf1c8ca70a0799b74fb23b449c2fc7
DawsonEpp <daws12@me.com>
2025-01-26 11:23:50 -0600

    multiplayer working??

--- bubbler-/scripts/game.gd
+++ bubbler-/scripts/game.gd
@@ -2,6 +2,8 @@ class_name Game
 extends Node
 
 @onready var multiplayer_ui = $UI/Multiplayer
+@onready var oid_lbl = $UI/Multiplayer/VBoxContainer/OID
+@onready var oid_input = $UI/Multiplayer/VBoxContainer/OIDInput
 
 const PLAYER = preload("res://scene/player.tscn")
 
@@ -11,10 +13,12 @@ var bullets: Array[bullet] = []
 
 func _ready():
 	$MultiplayerSpawner.spawn_function = add_player
+	
+	await Multiplayer.noray_connected
+	oid_lbl.text = Noray.oid
 
 func _on_host_pressed():
-	peer.create_server(25565)
-	multiplayer.multiplayer_peer = peer
+	Multiplayer.host()
 	
 	multiplayer.peer_connected.connect(
 		func(pid):
@@ -26,8 +30,8 @@ func _on_host_pressed():
 	multiplayer_ui.hide()
 
 func _on_join_pressed():
-	peer.create_client("localhost", 25565)
-	multiplayer.multiplayer_peer = peer
+	Multiplayer.join(oid_input.text)
+	#$MultiplayerSpawner.spawn(multiplayer.get_unique_id())
 	multiplayer_ui.hide()
 
 func add_player(pid):
@@ -41,3 +45,7 @@ func add_player(pid):
 
 func get_random_spawnpoint():
 	return $Level.get_children().pick_random().global_position
+
+
+func _on_copy_oid_pressed() -> void:
+	DisplayServer.clipboard_set(Noray.oid)
