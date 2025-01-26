5ac9ac5e14edf2e2dfbb46d39f0677984250d8df
DawsonEpp <daws12@me.com>
2025-01-26 09:18:34 -0600

    player sync works maybe

---
+++ bubbler-/scripts/game.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var multiplayer_ui = $UI/Multiplayer
+
+const PLAYER = preload("res://scene/player.tscn")
+
+var peer = ENetMultiplayerPeer.new()
+
+func _on_host_pressed():
+	peer.create_server(25565)
+	multiplayer.multiplayer_peer = peer
+	
+	multiplayer.peer_connected.connect(
+		func(pid):
+			print("Peer " + str(pid) + " has joined the game!")
+			add_player(pid)
+	)
+	
+	add_player(multiplayer.get_unique_id())
+	#multiplayer_ui.hide()
+
+func _on_join_pressed():
+	peer.create_client("localhost", 25565)
+	multiplayer.multiplayer_peer = peer
+	#multiplayer_ui.hide()
+
+func add_player(pid):
+	var player = PLAYER.instantiate()
+	player.name = str(pid)
+	add_child(player)
