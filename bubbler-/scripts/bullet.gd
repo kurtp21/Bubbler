71bdd2047304bf95c80883fa8192cdf731af774a
Kurt Pagal <kurtpagal@gmail.com>
2025-01-26 14:18:03 -0600

    Very buggy

--- bubbler-/scripts/bullet.gd
+++ bubbler-/scripts/bullet.gd
@@ -12,28 +12,42 @@ const SPEED = 500
 func _ready() -> void:
 	$Sprite2D.self_modulate.a = 1 # proof of opacity
 
+@rpc("any_peer")
+func sync_positions(new_pos: Vector2, new_rotation: float) -> void:
+	position = new_pos
+	rotation = new_rotation
+	
 func _process(delta: float) -> void:
-	pos = position
-	# Calculate movement vector based on rotation
-	direction = Vector2(cos(rotation), sin(rotation))
-	position += direction * SPEED * delta
-
-
-
+	if is_multiplayer_authority():
+		pos = position
+		direction = Vector2(cos(rotation), sin(rotation))
+		position += direction * SPEED * delta
+		sync_positions.rpc(position, rotation)
 
 func _on_body_entered(body):
 	if !is_multiplayer_authority():
 		return
-	
 	if body is Player:
 		body.take_damage.rpc_id(body.get_multiplayer_authority(), 1)
 		remove_bullet.rpc()
-	if body is WorldBoundaryShape2D:
-		remove_bullet.rpc()
-		print(1)
-	
-	
 
 @rpc("call_local")
 func remove_bullet():
 	queue_free()
+	#
+#func _process(delta: float) -> void:
+	#pos = position
+	## Calculate movement vector based on rotation
+	#direction = Vector2(cos(rotation), sin(rotation))
+	#position += direction * SPEED * delta
+#
+#func _on_body_entered(body): 
+	#if !is_multiplayer_authority():
+		#return
+	#if body is Player:
+		#body.take_damage.rpc_id(body.get_multiplayer_authority(), 1)
+		#remove_bullet.rpc()
+#
+#@rpc("call_local")
+#func remove_bullet():
+	#queue_free()
